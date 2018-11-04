d9200131a7b6cf1e25ba9509c1856c649af09403
fanny <fannythebanny@hotmail.com>
2018-11-03 19:10:37 -0600

    Testing out the CapacitySensor library

--- InteractivePiano/InteractivePiano.ino
+++ InteractivePiano/InteractivePiano.ino
@@ -1,9 +1,25 @@
+#include <CapacitiveSensor.h>
+CapacitiveSensor   cs_2_3 = CapacitiveSensor(2,3);        // 10M resistor between pins 2 & 3, pin 2 is sensor pin, add a wire and or foil if desired
+CapacitiveSensor   cs_2_4 = CapacitiveSensor(2,4);        // 10M resistor between pins 2 & 4, pin 2 is sensor pin, add a wire and or foil if desired
+CapacitiveSensor   cs_2_5 = CapacitiveSensor(2,5);        // 10M resistor between pins 2 & 5, pin 2 is sensor pin, add a wire and or foil if desired
 void setup() {
-  // put your setup code here, to run once:
-
+  cs_2_3.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+  
+  // Arduino start communicate with computer.
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    // Set the sensitivity of the sensors.
+    long total1 =  cs_2_3.capacitiveSensor(30);
+    long total2 =  cs_2_4.capacitiveSensor(30);
+    long total3 =  cs_2_5.capacitiveSensor(30);
+  
+    Serial.print(total1);                  // print sensor output 1
+    Serial.print("\n");
+    Serial.print(total2);                  // print sensor output 2
+    Serial.print("\n");
+    Serial.print(total3);                  // print sensor output 3
+    Serial.print("\n");
+    delay(1000);
 }
